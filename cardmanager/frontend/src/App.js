ca0682eeae68b83a21ee78c39f14dd0e1792f5b4
Christos Pasalidis <cpasalidis@gmail.com>
2018-04-08 10:34:26 +0300

    i can see react via django

--- cardmanager/frontend/src/App.js
+++ cardmanager/frontend/src/App.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
+import {Route, Switch, BrowserRouter} from 'react-router-dom';
+import Card from "./components/Card";
+import NotFound from "./components/NotFound";
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Card} />
+        <Route component={NotFound} />
+      </Switch>
+      </BrowserRouter>
     );
   }
 }
